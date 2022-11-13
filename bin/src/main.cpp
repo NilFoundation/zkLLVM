05cfe924cf3f7ff92a4db608324f4e228199941c
nkaskov <mail.nikita.kaskov@gmail.com>
2022-11-13 22:22:48 +0200

    Circuit marshalling to bytelob added. Submodules zk, zk-marshalling, zkLLVM-blueprint updated.

--- bin/src/main.cpp
+++ bin/src/main.cpp
@@ -16,10 +16,17 @@
 //---------------------------------------------------------------------------//
 
 #include <cstdint>
+#include <fstream>
 
 #include <nil/crypto3/algebra/curves/pallas.hpp>
 
 #include <nil/crypto3/zk/snark/arithmetization/plonk/params.hpp>
+#include <nil/crypto3/zk/snark/arithmetization/plonk/constraint_system.hpp>
+
+#include <nil/marshalling/status_type.hpp>
+#include <nil/marshalling/field_type.hpp>
+#include <nil/marshalling/endianness.hpp>
+#include <nil/crypto3/marshalling/zk/types/plonk/constraint_system.hpp>
 
 #include <nil/blueprint/parser.hpp>
 #include <nil/blueprint/utils/table_profiling.hpp>
@@ -27,6 +34,34 @@
 
 using namespace nil;
 using namespace nil::crypto3;
+
+template<typename TIter>
+void print_hex_byteblob(std::ostream &os, TIter iter_begin, TIter iter_end, bool endl) {
+    os << "0x" << std::hex;
+    for (TIter it = iter_begin; it != iter_end; it++) {
+        os << std::setfill('0') << std::setw(2) << std::right << int(*it);
+    }
+    os << std::dec;
+    if (endl) {
+        os << std::endl;
+    }
+}
+
+template<typename Endianness, typename ConstraintSystemType>
+void print_circuit(const ConstraintSystemType &circuit, std::string output_file) {
+
+    using TTypeBase = nil::marshalling::field_type<Endianness>;
+    auto filled_val = nil::crypto3::marshalling::types::fill_plonk_constraint_system<ConstraintSystemType, Endianness>(
+        circuit);
+
+    std::vector<std::uint8_t> cv;
+    cv.resize(filled_val.length(), 0x00);
+    auto write_iter = cv.begin();
+    nil::marshalling::status_type status = filled_val.write(write_iter, cv.size());
+    std::ofstream out;
+    out.open(output_file);
+    print_hex_byteblob(out, cv.cbegin(), cv.cend(), false);
+}
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -43,7 +78,8 @@ int main(int argc, char *argv[]) {
 
     using ArithmetizationParams =
         zk::snark::plonk_arithmetization_params<WitnessColumns, PublicInputColumns, ConstantColumns, SelectorColumns>;
-
+    using ConstraintSystemType =
+        zk::snark::plonk_constraint_system<BlueprintFieldType, ArithmetizationParams>;
 
     std::vector<typename BlueprintFieldType::value_type> public_input = {1, 11};
     nil::blueprint::parser<BlueprintFieldType, ArithmetizationParams> parser_instance;
@@ -64,6 +100,8 @@ int main(int argc, char *argv[]) {
     std::cout << "Padded rows: " << desc.rows_amount << std::endl;
 
     nil::blueprint::profiling(parser_instance.assignmnt);
+
+    print_circuit<nil::marshalling::option::big_endian, ConstraintSystemType>(parser_instance.bp, "circuit.bin");
 
     return !nil::blueprint::is_satisfied(parser_instance.bp, parser_instance.assignmnt);
 }
