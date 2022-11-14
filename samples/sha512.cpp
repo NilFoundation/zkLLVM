ed083a451f8ab7b42248a7e9d0d261adafc1bb40
nkaskov <mail.nikita.kaskov@gmail.com>
2022-11-14 11:09:10 +0200

    Hashes samples updated. SHA512 sample added.

---
+++ samples/sha512.cpp
@@ -0,0 +1,27 @@
+
+namespace nil{
+    namespace crypto3{
+        namespace algebra {
+            struct bls12381{
+                typedef field_bls12381b value_type;
+            };
+        }
+
+        namespace hashes {
+            typedef __attribute__((ext_vector_type(8))) typename algebra::bls12381::value_type sha512_state_type;
+            typedef __attribute__((ext_vector_type(16))) typename algebra::bls12381::value_type sha512_input_words_type;
+
+            sha512_state_type sha512(sha512_state_type input_state, sha512_input_words_type input_words);
+        }
+    }
+}
+    
+using nil::crypto3;
+
+typename hashes::sha512_state_type sha512_example(
+    hashes::sha512_state_type input_state, hashes::sha512_input_words_type input_words) {
+
+    typename hashes::sha512_state_type hash_result = hashes::sha512(input_state, input_words);
+
+    return hash_result;
+}
