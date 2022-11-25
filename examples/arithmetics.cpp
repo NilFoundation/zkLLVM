c118f1850ca2dc43534f19a2396cd9b1a7dfd601
Mikhail Aksenov <maksenov@nil.foundation>
2022-11-25 13:59:51 +0000

    Support call instruction handling in assinger

--- examples/arithmetics.cpp
+++ examples/arithmetics.cpp
@@ -16,11 +16,15 @@ namespace nil{
 
 using namespace nil::crypto3::algebra::curves;
 using namespace nil::crypto3::algebra;
+typename pallas::base_field_type square(typename pallas::base_field_type a)
+{
+    return a * a;
+}
 
 [[circuit]] typename pallas::base_field_type field_arithmetic_example(
 	typename pallas::base_field_type a,
 	typename pallas::base_field_type b) {
 
     typename pallas::base_field_type c = (a + b)*a + b*(a+b)*(a+b);
-    return c*c*c/(b - a);
+    return c*c*c/(b - a) + square(a);
 }
