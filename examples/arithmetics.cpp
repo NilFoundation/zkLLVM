f0dbe882a9a5bb2967fe1a5a4df025cc1ee6893d
Mikhail Aksenov <maksenov@nil.foundation>
2023-01-09 14:27:51 +0000

    Example with long constant initialization

--- examples/arithmetics.cpp
+++ examples/arithmetics.cpp
@@ -1,4 +1,5 @@
 #include <nil/crypto3/algebra/curves/pallas.hpp>
+#include "utility.hpp"
 
 using namespace nil::crypto3::algebra::curves;
 typename pallas::base_field_type::value_type square(
@@ -11,5 +12,6 @@ typename pallas::base_field_type::value_type square(
 	typename pallas::base_field_type::value_type b) {
 
     typename pallas::base_field_type::value_type c = (a + b)*a + b*(a+b)*(a+b);
-    return c*c*c/(b - a) + square(a);
+    const typename pallas::base_field_type::value_type constant = 0x12345678901234567890_cpui255;
+    return c*c*c/(b - a) + square(a) + constant;
 }
---
+++ examples/utility.hpp
@@ -0,0 +1,8 @@
+#ifndef ZKLLVM_STDLIB_UTILITY_H
+#define ZKLLVM_STDLIB_UTILITY_H
+
+inline constexpr const char *operator"" _cpui255(const char *val) {
+    return val;
+}
+
+#endif  // ZKLLVM_STDLIB_UTILITY_H
