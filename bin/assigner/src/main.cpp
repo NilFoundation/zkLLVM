046d6ec29dd90a8eb354087f985eda795bbef4b8
root <mail.nikita.kaskov@gmail.com>
2023-01-14 14:15:47 +0000

    Static selectors-compatible version introduced. #20

--- bin/assigner/src/main.cpp
+++ bin/assigner/src/main.cpp
@@ -131,9 +131,10 @@ int main(int argc, char *argv[]) {
     constexpr std::size_t WitnessColumns = 15;
     constexpr std::size_t PublicInputColumns = 5;
     constexpr std::size_t ConstantColumns = 5;
+    constexpr std::size_t SelectorColumns = 50;
 
     using ArithmetizationParams =
-        zk::snark::plonk_arithmetization_params<WitnessColumns, PublicInputColumns, ConstantColumns>;
+        zk::snark::plonk_arithmetization_params<WitnessColumns, PublicInputColumns, ConstantColumns, SelectorColumns>;
     using ConstraintSystemType = zk::snark::plonk_constraint_system<BlueprintFieldType, ArithmetizationParams>;
 
     std::vector<typename BlueprintFieldType::value_type> public_input;
@@ -162,7 +163,6 @@ int main(int argc, char *argv[]) {
     zk::snark::plonk_table_description<BlueprintFieldType, ArithmetizationParams> desc;
     desc.usable_rows_amount = parser_instance.assignmnt.rows_amount();
     desc.rows_amount = zk::snark::basic_padding(parser_instance.assignmnt);
-    desc.selector_columns = parser_instance.assignmnt.selectors_amount();
 
     std::ofstream otable;
     otable.open(assignment_table_file_name);
