0245b99c5c6080eca12747254c1d2d780ec06c6d
Mikhail Aksenov <maksenov@nil.foundation>
2023-01-16 09:30:28 +0000

    Add example of a loop

--- examples/arithmetics.cpp
+++ examples/arithmetics.cpp
@@ -2,9 +2,16 @@
 #include "utility.hpp"
 
 using namespace nil::crypto3::algebra::curves;
-typename pallas::base_field_type::value_type square(
-    typename pallas::base_field_type::value_type a) {
-    return a * a;
+typename pallas::base_field_type::value_type pow(
+    typename pallas::base_field_type::value_type a, int n) {
+    if (n == 0)
+        return 1;
+
+    typename pallas::base_field_type::value_type res = 1;
+    for (int i = 0; i < n; ++i) {
+        res *= a;
+    }
+    return res;
 }
 
 [[circuit]] typename pallas::base_field_type::value_type field_arithmetic_example(
@@ -13,5 +20,5 @@ typename pallas::base_field_type::value_type square(
 
     typename pallas::base_field_type::value_type c = (a + b)*a + b*(a+b)*(a+b);
     const typename pallas::base_field_type::value_type constant = 0x12345678901234567890_cpui255;
-    return c*c*c/(b - a) + square(a) + constant;
+    return c*c*c/(b - a) + pow(a, 2) + constant;
 }
