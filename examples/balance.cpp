f4163318698a2cb8a4bbf0ec3f84bae8e43eaea0
Mikhail Aksenov <maksenov@nil.foundation>
2023-02-17 12:53:35 +0200

    Add example with memory

---
+++ examples/balance.cpp
@@ -0,0 +1,24 @@
+#include <nil/crypto3/algebra/curves/pallas.hpp>
+
+using namespace nil::crypto3::algebra::curves;
+
+constexpr std::size_t VertexAmount = 5;
+[[circuit]] std::array<typename pallas::base_field_type::value_type, 2> balance(
+    std::array<typename pallas::base_field_type::value_type, 2*VertexAmount> vertexes,
+    std::array<typename pallas::base_field_type::value_type, VertexAmount> weights){
+
+    std::array<typename pallas::base_field_type::value_type, 2> balance_point;
+    balance_point[0] = balance_point[1] = static_cast<pallas::base_field_type::value_type>(0);
+    typename pallas::base_field_type::value_type balance_weight = 0;
+
+    for (std::size_t VertexIndex = 0; VertexIndex < VertexAmount; VertexIndex++){
+        balance_point[0] += vertexes[VertexIndex + VertexIndex] * weights[VertexIndex];
+        balance_point[1] += vertexes[VertexIndex + VertexIndex + 1] * weights[VertexIndex];
+        balance_weight += weights[VertexIndex];
+    }
+
+    balance_point[0] /= balance_weight;
+    balance_point[1] /= balance_weight;
+
+    return balance_point;
+}
