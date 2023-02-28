ea918fcd0f64fe578922269e4dcec98479408d87
Mikhail Komarov <nemo@nil.foundation>
2023-02-27 21:13:09 -0700

    Missing SDK submodules introduced. Build configuration updated.

--- cmake/CircuitCompile.cmake
+++ cmake/CircuitCompile.cmake
@@ -8,49 +8,39 @@
 # http://www.boost.org/LICENSE_1_0.txt
 #---------------------------------------------------------------------------#
 
-set(CIRCUIT_DEPENDENCIES
-    crypto3::algebra
-    crypto3::block
-    crypto3::blueprint
-    crypto3::codec
-    crypto3::math
-    crypto3::multiprecision
-    crypto3::pkpad
-    crypto3::pubkey
-    crypto3::random
-    crypto3::zk
-    crypto3::assigner
-    marshalling::core
-    marshalling::crypto3_algebra
-    marshalling::crypto3_multiprecision
-    marshalling::crypto3_zk)
-
 function(add_circuit name)
     set(prefix ARG)
     set(noValues "")
-    set(singleValues SOURCE)
-    set(multiValues INCLUDE_DIRECTORIES)
+    set(singleValues)
+    set(multiValues SOURCES INCLUDE_DIRECTORIES LINK_LIBRARIES)
     cmake_parse_arguments(${prefix}
                           "${noValues}"
                           "${singleValues}"
                           "${multiValues}"
                           ${ARGN})
 
-    if(NOT ARG_SOURCE)
-        message(FATAL_ERROR "SOURCE for ${name} circuit was not defined")
+    if(NOT ARG_SOURCES)
+        message(FATAL_ERROR "SOURCES for ${name} circuit was not defined")
     endif()
 
-    set(passed_source ${ARG_SOURCE})
-    if(NOT IS_ABSOLUTE ${ARG_SOURCE})
-        set(ARG_SOURCE "${CMAKE_CURRENT_SOURCE_DIR}/${ARG_SOURCE}")
-    endif()
-    if(NOT EXISTS ${ARG_SOURCE})
-        message(SEND_ERROR "Cannot find circuit source file: ${passed_source}")
-    endif()
+    foreach(source ${ARG_SOURCES})
+        if(NOT IS_ABSOLUTE ${include_dir})
+            list(APPEND CIRCUIT_SOURCES "${CMAKE_CURRENT_SOURCE_DIR}/${source}")
+        else()
+            list(APPEND CIRCUIT_SOURCES "${source}")
+        endif()
+    endforeach()
+    list(REMOVE_DUPLICATES CIRCUIT_SOURCES)
+
+    foreach(ITR ${CIRCUIT_SOURCES})
+        if(NOT EXISTS ${ITR})
+            message(SEND_ERROR "Cannot find circuit source file: ${source}")
+        endif()
+    endforeach()
 
     set(INCLUDE_DIRS_LIST "")
     # Collect include directories from dependencies first
-    foreach(lib ${CIRCUIT_DEPENDENCIES})
+    foreach(lib ${ARG_LINK_LIBRARIES})
         get_target_property(lib_include_dirs ${lib} INTERFACE_INCLUDE_DIRECTORIES)
         foreach(dir ${lib_include_dirs})
             list(APPEND INCLUDE_DIRS_LIST "-I${dir}")
@@ -64,7 +54,6 @@ function(add_circuit name)
         list(APPEND INCLUDE_DIRS_LIST "-I${include_dir}")
     endforeach()
     list(APPEND INCLUDE_DIRS_LIST -I${CMAKE_SOURCE_DIR}/libs/stdlib/libcpp -I${CMAKE_SOURCE_DIR}/libs/stdlib/libc/include)
-
     list(REMOVE_DUPLICATES INCLUDE_DIRS_LIST)
 
     if(CIRCUIT_ASSEMBLY_OUTPUT)
@@ -75,12 +64,12 @@ function(add_circuit name)
         set(format_option -c)
     endif()
 
-    add_custom_target(${name} COMMAND_EXPAND_LISTS VERBATIM
+    add_custom_target(${name}
+                      COMMAND $<TARGET_FILE:clang> -target assigner -Xclang -no-opaque-pointers
+                      -D__ZKLLVM__ ${INCLUDE_DIRS_LIST} -emit-llvm -O1 ${format_option} -o ${binary_name} ${CIRCUIT_SOURCES}
 
-                       COMMAND $<TARGET_FILE:clang> -target assigner -Xclang -no-opaque-pointers -D__ZKLLVM__ ${INCLUDE_DIRS_LIST} -emit-llvm -O1
-                       ${format_option} -o ${binary_name} ${ARG_SOURCE}
+                      VERBATIM COMMAND_EXPAND_LISTS
 
-                      SOURCES ${ARG_SOURCE})
-    set_target_properties(${name} PROPERTIES
-                          OUTPUT_NAME ${binary_name})
+                      SOURCES ${CIRCUIT_SOURCES})
+    set_target_properties(${name} PROPERTIES OUTPUT_NAME ${binary_name})
 endfunction(add_circuit)
