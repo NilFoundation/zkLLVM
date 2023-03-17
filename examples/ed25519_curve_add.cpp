9cd74e8695b25af6fb6d456e89c7f2a868be6da2
Mikhail Komarov <nemo@nil.foundation>
2023-03-17 15:33:20 +0200

    Examples changes #45

--- examples/ed25519_curve_add.cpp
+++ examples/ed25519_curve_add.cpp
@@ -1,3 +1,4 @@
-[[circuit]] __zkllvm_field_curve25519_base curve_operations(__zkllvm_field_curve25519_base a, __zkllvm_field_curve25519_base b) {
+[[circuit]] __zkllvm_field_curve25519_base curve_operations(__zkllvm_field_curve25519_base a,
+                                                            __zkllvm_field_curve25519_base b) {
     return a + b;
 }
--- examples/poseidon.cpp
+++ examples/poseidon.cpp
@@ -1,24 +1,24 @@
 #include <nil/crypto3/algebra/curves/pallas.hpp>
 
-namespace nil{
-    namespace crypto3{
+namespace nil {
+    namespace crypto3 {
         namespace hashes {
-            typedef __attribute__((ext_vector_type(3))) typename algebra::curves::pallas::base_field_type::value_type poseidon_block_type;
+            typedef __attribute__((ext_vector_type(3)))
+            typename algebra::curves::pallas::base_field_type::value_type poseidon_block_type;
 
             poseidon_block_type poseidon(poseidon_block_type input_block);
-        }
-    }
-}
+        }    // namespace hashes
+    }        // namespace crypto3
+}    // namespace nil
 
 using namespace nil::crypto3;
 using namespace nil::crypto3::algebra::curves;
 
-[[circuit]] typename hashes::poseidon_block_type poseidon_example1(
-    typename pallas::base_field_type::value_type a,
-    typename pallas::base_field_type::value_type b,
-    typename pallas::base_field_type::value_type c) {
+[[circuit]] typename hashes::poseidon_block_type poseidon_example1(typename pallas::base_field_type::value_type a,
+                                                                   typename pallas::base_field_type::value_type b,
+                                                                   typename pallas::base_field_type::value_type c) {
 
-    typename hashes::poseidon_block_type input_block = {a + b, b+c, a*c};
+    typename hashes::poseidon_block_type input_block = {a + b, b + c, a * c};
 
     typename hashes::poseidon_block_type hash_result = hashes::poseidon(input_block);
 
