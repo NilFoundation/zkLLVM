271db971512657a31e54dd9ca8d8c68d13d8414e
CblPOK-git <aleksandr.cherenkov.3@gmail.com>
2023-03-27 13:30:40 +0000

    added another version of merkle tree eposeidon example with the same problem  #52

--- examples/merkle_tree_poseidon.cpp
+++ examples/merkle_tree_poseidon.cpp
@@ -26,20 +26,20 @@ constexpr static const std::size_t leaves_size = input_size * 2 - 1;
     typename hashes::poseidon_block_type input_block = {input[0], input[1], input[2]};
     typename hashes::poseidon_block_type hash_result = hashes::poseidon(input_block);
 
-    // std::array<typename hashes::poseidon_block_type, input_size> input_blocks;
-    // std::array<typename hashes::poseidon_block_type, input_size> hash_results;
-
-    // for (std::size_t i = 0; i < input_size; i++) {
-    //     input_blocks[i] = {zero, zero, input[i]};
-    //     hash_results[i] = hashes::poseidon(input_blocks[i]);
-    //     leaves[i] = hash_results[i][0];
-    // }
+    std::array<typename hashes::poseidon_block_type, input_size> input_blocks;
+    std::array<typename hashes::poseidon_block_type, input_size> hash_results;
 
     for (std::size_t i = 0; i < input_size; i++) {
-        typename hashes::poseidon_block_type input_block2 = {zero, zero, input[i]};
-        typename hashes::poseidon_block_type hash_result2 = hashes::poseidon(input_block2);
-        leaves[i] = hash_result2[0];
+        input_blocks[i] = {zero, zero, input[i]};
+        hash_results[i] = hashes::poseidon(input_blocks[i]);
+        leaves[i] = hash_results[i][0];
     }
+
+    // for (std::size_t i = 0; i < input_size; i++) {
+    //     typename hashes::poseidon_block_type input_block2 = {zero, zero, input[i]};
+    //     typename hashes::poseidon_block_type hash_result2 = hashes::poseidon(input_block2);
+    //     leaves[i] = hash_result2[0];
+    // }
 
     return hash_result[0];
 }
