1fdf46289caa1f7179a79a23ba74f792ab1d3fe1
CblPOK-git <aleksandr.cherenkov.3@gmail.com>
2023-03-27 17:50:18 +0000

    merkle tree poseidon updated  #45

--- examples/merkle_tree_poseidon.cpp
+++ examples/merkle_tree_poseidon.cpp
@@ -14,42 +14,30 @@ namespace nil {
 using namespace nil::crypto3;
 using namespace nil::crypto3::algebra::curves;
 
-typename hashes::poseidon_block_type evaluate_poseidon(typename pallas::base_field_type::value_type a,
+typename pallas::base_field_type::value_type evaluate_poseidon(typename pallas::base_field_type::value_type a,
                                                         typename pallas::base_field_type::value_type b,
                                                         typename pallas::base_field_type::value_type c) {
     typename hashes::poseidon_block_type input_block = {a, b, c};
     typename hashes::poseidon_block_type hash_result = hashes::poseidon(input_block);
-    return hash_result;
-}
-
-typename pallas::base_field_type::value_type poseidon_two_inputs(typename pallas::base_field_type::value_type a,
-                                                                   typename pallas::base_field_type::value_type b) {
-    
-    typename pallas::base_field_type::value_type zero = 0;
-    typename hashes::poseidon_block_type hash_result = evaluate_poseidon(zero, a, b);
     return hash_result[2];
 }
 
-typename pallas::base_field_type::value_type poseidon_one_input(typename pallas::base_field_type::value_type a) {
-    typename pallas::base_field_type::value_type zero = 0;
-    typename hashes::poseidon_block_type hash_result = evaluate_poseidon(zero, zero, a);
-    return hash_result[2];
-}
-
-
-constexpr static const std::size_t input_size = 4;
+constexpr static const std::size_t input_log2 = 5;
+constexpr static const std::size_t input_size = 1 << input_log2;
 constexpr static const std::size_t leaves_size = input_size * 2 - 1;
 
 [[circuit]] pallas::base_field_type::value_type merkle_tree_poseidon (
     std::array<typename     algebra::curves::pallas::base_field_type::value_type, input_size> input) {
         std::array<typename algebra::curves::pallas::base_field_type::value_type, leaves_size> leaves;
 
+        typename pallas::base_field_type::value_type zero = 0;
+
         for (std::size_t i = 0; i < input_size; i++) {
-            leaves[i] = poseidon_one_input(input[i]);
+            leaves[i] = evaluate_poseidon(zero, input[i], zero);
         }
 
         for (std::size_t i = 0; i < input_size-1; i++) {
-            leaves[input_size + i] = poseidon_two_inputs(leaves[2*i], leaves[2*i + 1]);
+            leaves[input_size + i] = evaluate_poseidon(zero, leaves[i+i], leaves[i+i + 1]);
         }
 
         return leaves[leaves_size -1];
