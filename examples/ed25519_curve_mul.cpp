cb75874f1fbe50072613fe31d74d25c9a65a73ca
CblPOK-git <aleksandr.cherenkov.3@gmail.com>
2023-07-01 13:10:15 +0000

    update curve25519 field and curve examples

---
+++ examples/ed25519_curve_mul.cpp
@@ -0,0 +1,4 @@
+[[circuit]] __zkllvm_curve_curve25519 field_operations(__zkllvm_curve_curve25519 a,
+                                                            __zkllvm_field_curve25519_scalar b) {
+    return a * b;
+}
