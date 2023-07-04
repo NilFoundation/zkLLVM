b7212bcaac97bf7a2da942c01340eb1d96a353b9
CblPOK-git <aleksandr.cherenkov.3@gmail.com>
2023-07-04 07:54:59 +0000

    minor changes #127

--- examples/ed25519_curve_add.cpp
+++ examples/ed25519_curve_add.cpp
@@ -1,4 +1,4 @@
-[[circuit]] __zkllvm_curve_curve25519 field_operations(__zkllvm_curve_curve25519 a,
+[[circuit]] __zkllvm_curve_curve25519 curve_addition(__zkllvm_curve_curve25519 a,
                                                             __zkllvm_curve_curve25519 b) {
     return a + b;
 }
