ec0904cc9480353bf67ec2addde75dd0d90fe744
CblPOK-git <aleksandr.cherenkov.3@gmail.com>
2023-07-05 16:11:27 +0000

    add ed25519_field_add example, disable ed25519 curve add example #127

---
+++ examples/cpp/ed25519_field_add.cpp
@@ -0,0 +1,4 @@
+[[circuit]] __zkllvm_field_curve25519_base curve_addition(__zkllvm_field_curve25519_base a,
+                                                            __zkllvm_field_curve25519_base b) {
+    return a + b;
+}
