3fb9c043063a85d4d8c94db470469edce5b6ee43
root <mail.nikita.kaskov@gmail.com>
2023-07-11 08:50:18 +0000

    Oudated ed25519 field addition example removed.

--- examples/cpp/ed25519_field_add.cpp
+++ examples/cpp/ed25519_field_add.cpp
@@ -1,4 +1,4 @@
-[[circuit]] __zkllvm_field_curve25519_base curve_addition(__zkllvm_field_curve25519_base a,
+[[circuit]] __zkllvm_field_curve25519_base field_operations(__zkllvm_field_curve25519_base a,
                                                             __zkllvm_field_curve25519_base b) {
     return a + b;
 }
