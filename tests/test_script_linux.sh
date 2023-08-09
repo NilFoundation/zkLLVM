bf16b7cbff466167d8d8cb109353caae01c71aa8
CblPOK-git <aleksandr.cherenkov.3@gmail.com>
2023-08-09 19:03:33 +0000

    Update test targets generation, add test script, add tests into CI #181

---
+++ tests/test_script_linux.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+            test_examples=( \
+              "algebra/fields/pallas_add" \
+              "algebra/fields/pallas_mul" \
+            );
+
+            for test_example in ${test_examples[*]}; do
+              make -C build ${test_example////_}_calculate_expected_res  -j$(nproc);
+              make -C build ${test_example////_}  -j$(nproc);
+            done
+
+            for test_example in ${test_examples[*]}; do
+              for file in tests/inputs/algebra/fields/*.inp; do
+
+              echo -n "Test ${test_example}, input $file:";
+              ./build/tests/cpp/${test_example////_}_calculate_expected_res "$file" > expected.log
+              ./build/bin/assigner/assigner  -b build/tests/cpp/${test_example////_}.ll -i "$file"  -t assignment.tbl -c circuit.crct -e pallas --check  > real.log
+              diff -u expected.log real.log >diff.log;
+
+              if [ $? -eq 0 ]; then
+                echo -e "\033[32m succeeded \033[0m";
+              else
+                echo -e "\033[31m failed! \033[0m";
+                echo -e "\nexpected result:";
+                cat expected.log;
+                echo -e "\nreal circuit output:";
+                cat real.log;
+                echo -e "\ninput:";
+                cat "$file";
+                echo -e "\n--------------------------------------------------------------------------------";
+              fi
+              done
+            done
