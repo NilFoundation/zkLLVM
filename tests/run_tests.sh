1825dc042ad406381a9903772e03194d269b71da
CblPOK-git <aleksandr.cherenkov.3@gmail.com>
2023-08-19 21:15:00 +0000

    add bit shift tests, little cleanup in CMakelists and test_script #181

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,33 +1,18 @@
 #!/bin/bash
 
 readarray test_examples < tests/tests_list.txt
-readarray input_dirs_list < tests/input_dirs_list.txt
 exit_code=0
-
-            # shift_examples=( \
-            #   "algebra/integers/bit_operations/left_shift" \
-            #   "algebra/integers/bit_operations/right_shift" \
-            # );
-
-            # comparison_examples=( \
-            #   "algebra/integers/comparison/more" \
-            #   "algebra/integers/comparison/less" \
-            # );
 
 for i in ${!test_examples[*]}; do
   test_example=${test_examples[i]//$'\n'/}
   parent_dir=$(dirname "$test_example")
 
+  echo ""
   echo test_example: $test_example
-  echo parent_dir: $parent_dir
 
-  inputs_directory=${input_dirs_list[i]//$'\n'/}
-  # ls ${inputs_directory}
-
-  qwerty=${inputs_directory//$'\n'/}
   for file in tests/inputs/$parent_dir/*.inp; do
 
-  echo -n "Test ${test_example}, input $file: ";
+  echo -n "input $file: ";
   ./build/bin/assigner/assigner  -b build/tests/cpp/${test_example////_}.ll -i "$file"  -t assignment.tbl -c circuit.crct -e pallas --check  > real.log
   if [ $? -ne 0 ]; then
     exit_code=1
@@ -42,7 +27,6 @@ for i in ${!test_examples[*]}; do
     continue
   fi
 
-  # echo "16" > real.log
   diff -u expected.log real.log >diff.log;
 
   if [ $? -eq 0 ]; then
