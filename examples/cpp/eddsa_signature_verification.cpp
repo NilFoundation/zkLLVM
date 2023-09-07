56b046ee84855b8c580c4727486b134dd0efb926
CblPOK-git <aleksandr.cherenkov.3@gmail.com>
2023-09-07 20:49:08 +0000

    Add valid input for eddsa signature examle, enable temporarily disabled features that work now #234

--- examples/cpp/eddsa_signature_verification.cpp
+++ examples/cpp/eddsa_signature_verification.cpp
@@ -1,3 +1,6 @@
+#include <nil/crypto3/algebra/fields/curve25519/base_field.hpp>
+#include <nil/crypto3/algebra/fields/curve25519/scalar_field.hpp>
+#include <nil/crypto3/algebra/curves/ed25519.hpp>
 #include <nil/crypto3/algebra/curves/pallas.hpp>
 
 using namespace nil::crypto3::algebra::curves;
@@ -6,14 +9,14 @@ typedef __attribute__((ext_vector_type(4)))
                 typename pallas::base_field_type::value_type eddsa_message_block_type;
 
 [[circuit]] bool verify_eddsa_signature (
-    
-    __zkllvm_curve_curve25519 input_R, 
-    __zkllvm_field_curve25519_scalar input_s, 
-    __zkllvm_curve_curve25519 pk,
-    eddsa_message_block_type M,
-    __zkllvm_curve_curve25519 B) { // TODO replace with constructor
 
-        __zkllvm_field_curve25519_scalar k = __builtin_assigner_sha2_512_curve25519(input_R, pk, M);
+    typename ed25519::template g1_type<>::value_type input_R,
+    typename ed25519::scalar_field_type::value_type input_s,
+    typename ed25519::template g1_type<>::value_type pk,
+    eddsa_message_block_type M) {
 
-        return (B*input_s - (input_R + (pk*k))).is_zero()
+        typename ed25519::template g1_type<>::value_type B = ed25519::template g1_type<>::one();
+        typename ed25519::scalar_field_type::value_type k = __builtin_assigner_sha2_512_curve25519(input_R, pk, M);
+
+        return B*input_s == (input_R + (pk*k));
     }
