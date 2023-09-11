bb8385c98df666e1505ddd50a290d873a1ad9ab0
CblPOK-git <aleksandr.cherenkov.3@gmail.com>
2023-09-11 21:07:04 +0000

    add tests for ed25519 field arithmetics #181

--- tests/cpp/algebra/fields/25519_arithmetics/sub.cpp
+++ tests/cpp/algebra/fields/25519_arithmetics/sub.cpp
@@ -3,15 +3,16 @@
 #include <fstream>
 #endif
 
-#include <nil/crypto3/algebra/curves/pallas.hpp>
+#include <nil/crypto3/algebra/fields/curve25519/base_field.hpp>
+#include <nil/crypto3/algebra/curves/ed25519.hpp>
 
 using namespace nil::crypto3::algebra::curves;
 
-[[circuit]] typename pallas::base_field_type::value_type
-    pallas_field_add(typename pallas::base_field_type::value_type a,
-                        typename pallas::base_field_type::value_type b) {
+[[circuit]] typename ed25519::base_field_type::value_type field_addition(
+        typename ed25519::base_field_type::value_type a,
+        typename ed25519::base_field_type::value_type b) {
 
-    typename pallas::base_field_type::value_type c = a + b;
+    typename ed25519::base_field_type::value_type c = a - b;
 
     #ifndef __ZKLLVM__
     std::cout << c.data <<std::endl;
@@ -32,15 +33,13 @@ int main (int argc, char *argv[]){
 
     constexpr static const std::size_t input_size = 2;
 
-    using BlueprintFieldType = typename pallas::base_field_type;
-
-    std::vector<typename BlueprintFieldType::value_type> input_vec = read_fields<BlueprintFieldType>(input_file_name);
+    std::vector<typename ed25519::base_field_type::value_type> input_vec = read_fields<typename ed25519::base_field_type>(input_file_name);
     if (input_vec.size() != input_size){
         std::cerr << "input file contains " << input_vec.size() << " field elements" << "\n";
         std::cerr << "addition circuit accepts " << input_size << " field elements" << "\n";
         assert(false && "input file does not match circuit signature");
     } else{
-        pallas_field_add(input_vec[0], input_vec[1]);
+        field_addition(input_vec[0], input_vec[1]);
         return 0;
     }
 }
