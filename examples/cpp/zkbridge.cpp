07810c3a2de9d8e8d8f6f0f02f263d4efca4f9c8
Mikhail Aksenov <maksenov@nil.foundation>
2023-09-11 13:57:50 +0000

    Assigner switched to new memory model
    Opaque pointers are reenabled. Set up initial building of stdlib

--- examples/cpp/zkbridge.cpp
+++ examples/cpp/zkbridge.cpp
@@ -8,19 +8,31 @@ using namespace nil::crypto3::algebra::curves;
 typedef __attribute__((ext_vector_type(4)))
                 typename pallas::base_field_type::value_type eddsa_message_block_type;
 
-bool verify_eddsa_signature (
-    __zkllvm_curve_curve25519 input_R, 
-    __zkllvm_field_curve25519_scalar input_s, 
+typedef struct __attribute__((packed)) {
+    __zkllvm_curve_curve25519 R;
+    __zkllvm_field_curve25519_scalar s;
+} eddsa_signature_type;
+
+bool verify_eddsa_signature (eddsa_signature_type input,
     __zkllvm_curve_curve25519 pk,
     eddsa_message_block_type M) {
 
-        // __zkllvm_curve_curve25519 B = 1; // TODO
-        __zkllvm_curve_curve25519 B = input_R;
-        __zkllvm_field_curve25519_scalar k = __builtin_assigner_sha2_512_curve25519(input_R, pk, M);
+        // __zkllvm_curve_curve25519 B = ed25519::template g1_type<>::value_type::one();
+        __zkllvm_curve_curve25519 B = pk;
+        __zkllvm_field_curve25519_scalar k = __builtin_assigner_sha2_512_curve25519(input.R, pk, M);
 
-        // return (B*input_s - (input_R + (pk*k))).is_zero(); // TODO
+        // return (B*input.s - (input.R + (pk*k))).is_zero();
         return true;
 }
+
+typedef struct __attribute__((packed)) {
+    typename hashes::sha2<256>::block_type prev_block_hash;
+    typename hashes::sha2<256>::block_type data;
+    std::array<eddsa_signature_type, 16> validators_signatures;
+    std::array<__zkllvm_curve_curve25519, 16> validators_keys;
+    // eddsa_signature_type validators_signature;
+    // __zkllvm_curve_curve25519 validators_key;
+} block_data_type;
 
 bool is_same(typename hashes::sha2<256>::block_type block0,
     typename hashes::sha2<256>::block_type block1){
@@ -30,30 +42,22 @@ bool is_same(typename hashes::sha2<256>::block_type block0,
 
 [[circuit]] bool verify_protocol_state_proof (
     typename hashes::sha2<256>::block_type last_confirmed_block_hash,
+    std::array<block_data_type, 64> unconfirmed_blocks) {
 
-    std::array< typename hashes::sha2<256>::block_type,            64> unconfirmed_blocks_prev_block_hash,
-    std::array< typename hashes::sha2<256>::block_type,            64> unconfirmed_blocks_data,
-    std::array< std::array< __zkllvm_curve_curve25519, 16>,        64> unconfirmed_blocks_validators_signatures_R,
-    std::array< std::array< __zkllvm_field_curve25519_scalar, 16>, 64> unconfirmed_blocks_validators_signatures_s,
-    std::array< std::array< __zkllvm_curve_curve25519, 16>,        64> unconfirmed_blocks_validators_keys,
-    std::array< __zkllvm_curve_curve25519,                         64> unconfirmed_blocks_validators_signature_R,
-    std::array< __zkllvm_field_curve25519_scalar,                  64> unconfirmed_blocks_validators_signature_s,
-    std::array< __zkllvm_curve_curve25519,                         64> unconfirmed_blocks_validators_key) {
-    
-    for (int i = 0; i < unconfirmed_blocks_prev_block_hash.size(); i++) {
-        
+    for (int i = 0; i < unconfirmed_blocks.size(); i++) {
+
         // Check hashes correctness
         if (i == 0) {
-            if (!is_same(unconfirmed_blocks_prev_block_hash[i],
+            if (!is_same(unconfirmed_blocks[i].prev_block_hash,
                 last_confirmed_block_hash)) {
                 return false;
             }
         } else {
             typename hashes::sha2<256>::block_type evaluated_block_hash =
-                hash<hashes::sha2<256>>(unconfirmed_blocks_prev_block_hash[i-1],
-                    unconfirmed_blocks_data[i-1]);
-            
-            if (!is_same(unconfirmed_blocks_prev_block_hash[i],
+                hash<hashes::sha2<256>>(unconfirmed_blocks[i-1].prev_block_hash,
+                    unconfirmed_blocks[i-1].data);
+
+            if (!is_same(unconfirmed_blocks[i].prev_block_hash,
                 evaluated_block_hash)) {
                 return false;
             }
@@ -61,15 +65,13 @@ bool is_same(typename hashes::sha2<256>::block_type block0,
 
         // Verify signatures
         for (int j = 0; j < 16; j++) {
-            eddsa_message_block_type message = {unconfirmed_blocks_prev_block_hash[i][0],
-                unconfirmed_blocks_prev_block_hash[i][1],
-                unconfirmed_blocks_data[i][0],
-                unconfirmed_blocks_data[i][1]};
+            eddsa_message_block_type message = {unconfirmed_blocks[i].prev_block_hash[0],
+                unconfirmed_blocks[i].prev_block_hash[1],
+                unconfirmed_blocks[i].data[0],
+                unconfirmed_blocks[i].data[1]};
 
-            if (!verify_eddsa_signature(
-                    unconfirmed_blocks_validators_signatures_R[i][j],
-                    unconfirmed_blocks_validators_signatures_s[i][j],
-                    unconfirmed_blocks_validators_keys[i][j],
+            if (!verify_eddsa_signature(unconfirmed_blocks[i].validators_signatures[j],
+                    unconfirmed_blocks[i].validators_keys[j],
                     message)) {
                 return false;
             }
