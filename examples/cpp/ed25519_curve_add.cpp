8eb2b18e6f8250e879f1bf1b6d3fcaef42a838fb
CblPOK-git <aleksandr.cherenkov.3@gmail.com>
2023-09-14 11:35:50 +0000

    small changes in examples #181

--- examples/cpp/ed25519_curve_add.cpp
+++ examples/cpp/ed25519_curve_add.cpp
@@ -1,4 +1,14 @@
-[[circuit]] __zkllvm_curve_curve25519 curve_addition(__zkllvm_curve_curve25519 a,
-                                                            __zkllvm_curve_curve25519 b) {
-    return a + b;
+#include <nil/crypto3/algebra/fields/curve25519/base_field.hpp>
+#include <nil/crypto3/algebra/fields/curve25519/scalar_field.hpp>
+#include <nil/crypto3/algebra/curves/ed25519.hpp>
+#include <nil/crypto3/algebra/curves/pallas.hpp>
+
+using namespace nil::crypto3::algebra::curves;
+
+[[circuit]] typename ed25519::template g1_type<>::value_type curve_addition(
+        typename ed25519::template g1_type<>::value_type a,
+        typename ed25519::template g1_type<>::value_type b) {
+    typename ed25519::template g1_type<>::value_type B = ed25519::template g1_type<>::zero();
+
+    return a + b + B;
 }
