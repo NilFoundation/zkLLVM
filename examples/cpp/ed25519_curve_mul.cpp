7efc1f3b55b20bcdb69d069f4e70752f76c19b99
CblPOK-git <aleksandr.cherenkov.3@gmail.com>
2023-09-15 21:48:52 +0000

    add tests for pallas curve multiplication, zero and one constructors #181

--- examples/cpp/ed25519_curve_mul.cpp
+++ examples/cpp/ed25519_curve_mul.cpp
@@ -1,4 +1,10 @@
-[[circuit]] __zkllvm_curve_curve25519 curve_multiplication(__zkllvm_curve_curve25519 a,
-                                                            __zkllvm_field_curve25519_scalar b) {
-    return a * b;
+#include <nil/crypto3/algebra/curves/ed25519.hpp>
+
+using namespace nil::crypto3::algebra::curves;
+
+[[circuit]] typename ed25519::template g1_type<nil::crypto3::algebra::curves::coordinates::affine>::value_type curve_mul(
+        typename ed25519::template g1_type<nil::crypto3::algebra::curves::coordinates::affine>::value_type point,
+        typename ed25519::scalar_field_type::value_type scalar) {
+
+    return point * scalar;
 }
---
+++ tests/cpp/algebra/curves/constructors/one_ed25519.cpp
@@ -0,0 +1,41 @@
+#ifndef __ZKLLVM__
+#include "../../../read_boost_json.hpp"
+#include <fstream>
+#endif
+
+#include <nil/crypto3/algebra/curves/ed25519.hpp>
+
+using namespace nil::crypto3::algebra::curves;
+
+[[circuit]] typename ed25519::template g1_type<nil::crypto3::algebra::curves::coordinates::affine>::value_type constructor_test() {
+
+#ifdef __ZKLLVM__
+    typename ed25519::template g1_type<nil::crypto3::algebra::curves::coordinates::affine>::value_type B =
+        ed25519::template g1_type<nil::crypto3::algebra::curves::coordinates::affine>::one();
+#else
+    typename ed25519::template g1_type<nil::crypto3::algebra::curves::coordinates::affine>::value_type B =
+        ed25519::template g1_type<nil::crypto3::algebra::curves::coordinates::affine>::value_type::one();
+        ed25519::template g1_type<nil::crypto3::algebra::curves::coordinates::affine>::value_type::one();
+#endif
+
+    #ifndef __ZKLLVM__
+    std::cout << B.X.data <<std::endl;
+    std::cout << B.Y.data <<std::endl;
+    #endif
+
+    return B;
+}
+
+#ifndef __ZKLLVM__
+
+int main (int argc, char *argv[]){
+    if (argc != 2) {
+        std::cerr << "one command line argument must be provided\n";
+        std::abort();
+    }
+
+    constructor_test();
+    return 0;
+}
+
+#endif
