6944eb6d63ab439c6b4f4c9476e4645eb0008437
CblPOK-git <aleksandr.cherenkov.3@gmail.com>
2023-09-15 21:48:52 +0000

    add tests for pallas curve multiplication, zero and one constructors #181

--- examples/cpp/pallas_curve_examples/pallas_curve_add.cpp
+++ examples/cpp/pallas_curve_examples/pallas_curve_add.cpp
@@ -1,3 +1,10 @@
-[[circuit]] __zkllvm_curve_pallas curve_operations(__zkllvm_curve_pallas a, __zkllvm_curve_pallas b) {
+#include <nil/crypto3/algebra/curves/pallas.hpp>
+
+using namespace nil::crypto3::algebra::curves;
+
+[[circuit]] typename pallas::template g1_type<nil::crypto3::algebra::curves::coordinates::affine>::value_type curve_operations(
+        typename pallas::template g1_type<nil::crypto3::algebra::curves::coordinates::affine>::value_type a,
+        typename pallas::template g1_type<nil::crypto3::algebra::curves::coordinates::affine>::value_type b) {
+
     return a + b;
 }
