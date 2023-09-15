200579e3f7f9aeaf746b3b3a71d84fc0cc8a71fa
CblPOK-git <aleksandr.cherenkov.3@gmail.com>
2023-09-15 21:48:52 +0000

    add tests for pallas curve multiplication, zero and one constructors #181

---
+++ tests/cpp/algebra/curves/constructors/one_pallas.cpp
@@ -0,0 +1,40 @@
+#ifndef __ZKLLVM__
+#include "../../../read_boost_json.hpp"
+#include <fstream>
+#endif
+
+#include <nil/crypto3/algebra/curves/pallas.hpp>
+
+using namespace nil::crypto3::algebra::curves;
+
+[[circuit]] typename pallas::template g1_type<nil::crypto3::algebra::curves::coordinates::affine>::value_type constructor_test() {
+
+#ifdef __ZKLLVM__
+    typename pallas::template g1_type<nil::crypto3::algebra::curves::coordinates::affine>::value_type B =
+        pallas::template g1_type<nil::crypto3::algebra::curves::coordinates::affine>::zero();
+#else
+    typename pallas::template g1_type<nil::crypto3::algebra::curves::coordinates::affine>::value_type B =
+        pallas::template g1_type<nil::crypto3::algebra::curves::coordinates::affine>::value_type::zero();
+#endif
+
+    #ifndef __ZKLLVM__
+    std::cout << B.X.data <<std::endl;
+    std::cout << B.Y.data <<std::endl;
+    #endif
+
+    return B;
+}
+
+#ifndef __ZKLLVM__
+
+int main (int argc, char *argv[]){
+    if (argc != 2) {
+        std::cerr << "one command line argument must be provided\n";
+        std::abort();
+    }
+
+    constructor_test();
+    return 0;
+}
+
+#endif
