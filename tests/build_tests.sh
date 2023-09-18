19b3fc8dc9c8cfee7b9ce9a78ebbdc656b804de0
CblPOK-git <aleksandr.cherenkov.3@gmail.com>
2023-09-18 19:56:08 +0000

    return 1 from build_tests if compilation of some target failed #181

--- tests/build_tests.sh
+++ tests/build_tests.sh
@@ -1,4 +1,5 @@
 readarray test_examples < tests/tests_list.txt
+exit_code=0
 
 if [ $1 != "make" ]  &&  [ $1 != "ninja" ]; then
   echo "Got $1 instead of make/ninja"
@@ -6,12 +7,24 @@ if [ $1 != "make" ]  &&  [ $1 != "ninja" ]; then
   exit 1
 fi
 
+
 if [[ $2 =~ ^-j[0-9]+$ ]]; then
   for test_example in ${test_examples[*]}; do
+
     $1 -C build ${test_example////_}_calculate_expected_res  $2;
+    if [ $? -ne 0 ]; then
+       exit_code=1
+    fi
+
     $1 -C build ${test_example////_}  $2;
+    if [ $? -ne 0 ]; then
+       exit_code=1
+    fi
+
   done
 else
   echo "Got $2 as command line argument 2, must be number of cpu's"
   echo "(linux: -j\$(nproc), mac: : -j\$(sysctl -n hw.logicalcpu))"
 fi
+
+exit $exit_code
