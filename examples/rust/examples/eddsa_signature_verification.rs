b3bfc629285cf8b2fc1ce67c22655e816c8eecb8
Alexander Evgin <aleasims@gmail.com>
2023-10-12 19:41:48 +0000

    Switch SHA2-512 examples to use arkworks types

--- examples/rust/examples/eddsa_signature_verification.rs
+++ examples/rust/examples/eddsa_signature_verification.rs
@@ -1,15 +1,26 @@
 #![no_main]
 
+use ark_curve25519::{EdwardsAffine, Fr};
+use ark_pallas::Fq;
+
 use std::intrinsics::assigner_sha2_512;
+
+type EdDSAMessageBlockType = [Fq; 4];
 
 #[circuit]
 pub fn verify_eddsa_signature(
-    input_r: __zkllvm_curve_curve25519,
-    input_s: __zkllvm_field_curve25519_scalar,
-    pk: __zkllvm_curve_curve25519,
-    m: [__zkllvm_field_pallas_base; 4],
+    input_r: EdwardsAffine,
+    input_s: Fr,
+    pk: EdwardsAffine,
+    m: EdDSAMessageBlockType,
 ) -> bool {
-    let b = __zkllvm_curve_curve25519::one();
-    let k = assigner_sha2_512(input_r, pk, m);
+    let b = EdwardsAffine::one(); // here must be `EdwardsAffine::gerenator()`
+    let k = hash(input_r, pk, m);
     b * input_s == input_r + (pk * k)
+}
+
+// This will be arkworks function here in the future.
+// All of the implementation details will be hidden from user.
+fn hash(r: EdwardsAffine, pk: EdwardsAffine, m: EdDSAMessageBlockType) -> Fr {
+    assigner_sha2_512(r.0, pk.0, [m[0].0, m[1].0, m[2].0, m[3].0]).into()
 }
