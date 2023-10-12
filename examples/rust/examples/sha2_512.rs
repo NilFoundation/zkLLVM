b7a3c6b03404db3ca2bdc3d38514d90c295c3f1e
Alexander Evgin <aleasims@gmail.com>
2023-10-12 19:41:48 +0000

    Switch SHA2-512 examples to use arkworks types

--- examples/rust/examples/sha2_512.rs
+++ examples/rust/examples/sha2_512.rs
@@ -1,14 +1,19 @@
 #![no_main]
 
+use ark_curve25519::{EdwardsAffine, Fr};
+use ark_pallas::Fq;
+
 use std::intrinsics::assigner_sha2_512;
 
-type BlockType = [__zkllvm_field_pallas_base; 4];
+type BlockType = [Fq; 4];
 
 #[circuit]
-pub fn sha512_example(
-    r: __zkllvm_curve_curve25519,
-    pk: __zkllvm_curve_curve25519,
-    m: BlockType,
-) -> __zkllvm_field_curve25519_scalar {
-    assigner_sha2_512(r, pk, m)
+pub fn sha512_example(r: EdwardsAffine, pk: EdwardsAffine, m: BlockType) -> Fr {
+    hash(r, pk, m)
+}
+
+// This will be arkworks function here in the future.
+// All of the implementation details will be hidden from user.
+fn hash(r: EdwardsAffine, pk: EdwardsAffine, m: BlockType) -> Fr {
+    assigner_sha2_512(r.0, pk.0, [m[0].0, m[1].0, m[2].0, m[3].0]).into()
 }
