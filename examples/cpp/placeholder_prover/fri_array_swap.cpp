0e9827eabab108e4ca85de117dfea375c06b2ed8
CblPOK-git <aleksandr.cherenkov.3@gmail.com>
2023-10-20 17:22:29 +0000

    add fri_cosets example #300

--- examples/cpp/placeholder_prover/fri_array_swap.cpp
+++ examples/cpp/placeholder_prover/fri_array_swap.cpp
@@ -2,16 +2,10 @@
 
 using namespace nil::crypto3::algebra::curves;
 
-constexpr const std::size_t size = 6;
-
-[[circuit]] std::array<typename pallas::base_field_type::value_type, size>
-    field_arithmetic_example(typename pallas::base_field_type::value_type swap_field,
-        std::array<typename pallas::base_field_type::value_type, size> input) {
-
-    bool swap = typename pallas::base_field_type::value_type(0) != swap_field;
-
-    std::array<typename pallas::base_field_type::value_type, size> res;
-    for (std::size_t i = 0; i < size; i+=2) {
+template<typename input_type>
+input_type swap_array(input_type input, bool swap) {
+    input_type res;
+    for (std::size_t i = 0; i < res.size(); i+=2) {
         if (swap) {
             res[i] = input[i+1];
             res[i+1] = input[i];
@@ -22,4 +16,26 @@ constexpr const std::size_t size = 6;
         }
     }
     return res;
+}
+
+
+[[circuit]] typename pallas::base_field_type::value_type
+    field_arithmetic_example(typename pallas::base_field_type::value_type swap_field,
+        std::array<typename pallas::base_field_type::value_type, 6> input) {
+
+    bool swap = typename pallas::base_field_type::value_type(0) != swap_field;
+
+    using arr6_type = std::array<typename pallas::base_field_type::value_type, 6>;
+    using arr4_type = std::array<typename pallas::base_field_type::value_type, 4>;
+
+    arr6_type arr1 = swap_array<arr6_type> (input, swap);
+    arr4_type arr2;
+
+    for (std::size_t i = 0; i < arr2.size(); i++) {
+        arr2[i] = arr1[i];
+    }
+
+    arr4_type arr3 = swap_array<arr4_type> (arr2, !swap);
+
+    return arr1[1] + arr2[2] + arr3[3];
 }
---
+++ examples/cpp/placeholder_prover/fri_cosets.cpp
@@ -0,0 +1,16 @@
+#include <nil/crypto3/algebra/curves/pallas.hpp>
+
+using namespace nil::crypto3::algebra::curves;
+
+    constexpr std::size_t bits_amount = 64;
+    constexpr std::size_t array_length = 3;
+    constexpr typename pallas::base_field_type::value_type omega = 0x1ff2863fd35bfc59e51f3693bf37e2d841d1b5fbed4138f755a638bec8750abd_cppui255;
+
+[[circuit]] std::array<std::array<typename pallas::base_field_type::value_type, 3>, array_length>
+    fri_cosets(typename pallas::base_field_type::value_type input) {
+
+    std::array<std::array<typename pallas::base_field_type::value_type, 3>, array_length> res;
+
+     __builtin_assigner_fri_cosets(res.data(), array_length, omega, bits_amount, input);
+     return res;
+}
