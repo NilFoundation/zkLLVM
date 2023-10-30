b4808a8dc5eecfa4390645aaa929521b7d4bcd26
Alexander Evgin <aleasims@gmail.com>
2023-10-30 14:24:52 +0000

    Update arithmetics example to be fully arkworks-built

--- examples/rust/examples/arithmetics_arkworks.rs
+++ examples/rust/examples/arithmetics_arkworks.rs
@@ -1,14 +1,17 @@
 #![no_main]
+#![feature(const_trait_impl)]
+#![feature(const_convert)]
 
+use ark_ff::{Field, MontFp};
 use ark_pallas::Fq;
 
 fn pow(a: Fq, n: u32) -> Fq {
-    (0..n).fold(1g.into(), |acc, _| acc * a)
+    (0..n).fold(Fq::ONE, |acc, _| acc * a)
 }
 
 #[circuit]
 pub fn field_arithmetic_example(a: Fq, b: Fq) -> Fq {
     let c = (a + b) * a + b * (a + b) * (a + b);
-    const CONSTANT: Fq = Fq(0x12345678901234567890g);
+    const CONSTANT: Fq = MontFp!("0x12345678901234567890");
     c * c * c / (b - a) + pow(a, 2) + CONSTANT
 }
