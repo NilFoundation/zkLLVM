bde8ee59db9faea3b08795f38a72c53ff80db036
Aleksandr Cherenkov <aleksandr.cherenkov.3@gmail.com>
2023-12-01 15:10:15 +0000

    rework tests for different curve types usage, add bls12381_fp12_mul_test #181

--- tests/cpp/bls_signature/non_aggregated/verification.cpp
+++ tests/cpp/bls_signature/non_aggregated/verification.cpp
@@ -8,9 +8,26 @@
 #include <nil/crypto3/algebra/curves/bls12.hpp>
 #include <nil/crypto3/algebra/algorithms/pair.hpp>
 
-
 using namespace nil::crypto3;
 using namespace nil::crypto3::algebra::curves;
+
+bool check_equality(
+    typename algebra::curves::bls12<381>::gt_type::value_type pairing1,
+    typename algebra::curves::bls12<381>::gt_type::value_type pairing2
+) {
+
+#ifdef __ZKLLVM__
+    bool are_equal = (pairing1[0] == pairing2[0]);
+    for(std::size_t i = 1; i < 12; i++) {
+        are_equal = are_equal && (pairing1[i] == pairing2[i]);
+    }
+#else
+    bool are_equal = (pairing1 == pairing2);
+
+    std::cout << are_equal <<std::endl;
+#endif
+    return are_equal;
+}
 
 [[circuit]] bool verify_signature(
     typename algebra::fields::bls12_base_field<381>::value_type hashed_msg,
@@ -18,31 +35,21 @@ using namespace nil::crypto3::algebra::curves;
     typename algebra::curves::bls12<381>::template g1_type<>::value_type sig) {
 
 #ifdef __ZKLLVM__
-        typename algebra::curves::bls12<381>::template g1_type<>::value_type msg_point = __builtin_assigner_hash_to_curve(hashed_msg);
+    typename algebra::curves::bls12<381>::template g1_type<>::value_type msg_point = __builtin_assigner_hash_to_curve(hashed_msg);
 
-        __builtin_assigner_is_in_g1_check(sig);
-        __builtin_assigner_is_in_g2_check(pubkey);
+    __builtin_assigner_is_in_g1_check(sig);
+    __builtin_assigner_is_in_g2_check(pubkey);
 
-        typename algebra::curves::bls12<381>::template g2_type<>::value_type g2_group_generator = algebra::curves::bls12<381>::template g2_type<>::one();
+    typename algebra::curves::bls12<381>::template g2_type<>::value_type g2_group_generator = algebra::curves::bls12<381>::template g2_type<>::one();
 #else
-        typename algebra::curves::bls12<381>::template g1_type<>::value_type msg_point;
-        typename algebra::curves::bls12<381>::template g2_type<>::value_type g2_group_generator = algebra::curves::bls12<381>::template g2_type<>::value_type::one();
+    typename algebra::curves::bls12<381>::template g1_type<>::value_type msg_point;
+    typename algebra::curves::bls12<381>::template g2_type<>::value_type g2_group_generator = algebra::curves::bls12<381>::template g2_type<>::value_type::one();
 #endif
 
+    typename algebra::curves::bls12<381>::gt_type::value_type pairing1 = algebra::pair<algebra::curves::bls12<381>>(sig, g2_group_generator);
+    typename algebra::curves::bls12<381>::gt_type::value_type pairing2 = algebra::pair<algebra::curves::bls12<381>>(msg_point, pubkey);
 
-        typename algebra::curves::bls12<381>::gt_type::value_type pairing1 = algebra::pair<algebra::curves::bls12<381>>(sig, g2_group_generator);
-        typename algebra::curves::bls12<381>::gt_type::value_type pairing2 = algebra::pair<algebra::curves::bls12<381>>(msg_point, pubkey);
-
-        bool are_equal = pairing1 == pairing2;
-#ifdef __ZKLLVM__
-        // __builtin_assigner_exit_check(are_equal);
-#endif
-
-#ifndef __ZKLLVM__
-        std::cout << are_equal <<std::endl;
-#endif
-
-        return are_equal;
+    return check_equality(pairing1, pairing2);
 }
 
 
