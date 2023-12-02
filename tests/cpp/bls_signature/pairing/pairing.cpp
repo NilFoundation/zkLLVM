f8f6e6c86635d660ab14bcd6fac8b7a35dccfc4c
Aleksandr Cherenkov <aleksandr.cherenkov.3@gmail.com>
2023-12-02 18:52:22 +0000

    add bls12381 pairing test #54

---
+++ tests/cpp/bls_signature/pairing/pairing.cpp
@@ -0,0 +1,60 @@
+#ifndef __ZKLLVM__
+#include "../../read_boost_json.hpp"
+#include <fstream>
+#include <nil/crypto3/algebra/pairing/bls12.hpp>
+#endif
+
+#include <nil/crypto3/algebra/fields/bls12/base_field.hpp>
+#include <nil/crypto3/algebra/curves/bls12.hpp>
+#include <nil/crypto3/algebra/algorithms/pair.hpp>
+
+using namespace nil::crypto3;
+using namespace nil::crypto3::algebra::curves;
+
+
+[[circuit]] typename algebra::curves::bls12<381>::gt_type::value_type pairing_test(
+    typename algebra::curves::bls12<381>::template g1_type<>::value_type g1,
+    typename algebra::curves::bls12<381>::template g2_type<>::value_type g2
+) {
+
+    typename algebra::curves::bls12<381>::gt_type::value_type res = algebra::pair<algebra::curves::bls12<381>>(g1, g2);
+
+#ifndef __ZKLLVM__
+    std::cout << res.data[0].data[0].data[0].data << "\n";
+    std::cout << res.data[0].data[0].data[1].data << "\n";
+    std::cout << res.data[0].data[1].data[0].data << "\n";
+    std::cout << res.data[0].data[1].data[1].data << "\n";
+    std::cout << res.data[0].data[2].data[0].data << "\n";
+    std::cout << res.data[0].data[2].data[1].data << "\n";
+    std::cout << res.data[1].data[0].data[0].data << "\n";
+    std::cout << res.data[1].data[0].data[1].data << "\n";
+    std::cout << res.data[1].data[1].data[0].data << "\n";
+    std::cout << res.data[1].data[1].data[1].data << "\n";
+    std::cout << res.data[1].data[2].data[0].data << "\n";
+    std::cout << res.data[1].data[2].data[1].data << "\n";
+#endif
+    return res;
+}
+
+
+#ifndef __ZKLLVM__
+int main (int argc, char *argv[]){
+    if (argc != 2) {
+        std::cerr << "one command line argument must be provided\n";
+        std::abort();
+    }
+
+    boost::json::value input_json = read_boost_json(std::string(argv[1]));
+
+    using blueprint_field_type = typename algebra::curves::bls12<381>::base_field_type;
+    using g2_group_type = typename bls12<381>::g2_type<>;
+    using curve_point_type = algebra::curves::bls12<381>::template g1_type<>::value_type;
+
+    curve_point_type g1 = read_curve<curve_point_type, blueprint_field_type>(input_json, 0);
+    g1.Z = bls12<381>::template g1_type<>::field_type::value_type::one();
+    g2_group_type::value_type g2 = read_g2_curve<blueprint_field_type, g2_group_type>(input_json, 1);
+
+    pairing_test(g1, g2);
+    return 0;
+}
+#endif
--- tests/cpp/read_boost_json.hpp
+++ tests/cpp/read_boost_json.hpp
@@ -232,6 +232,32 @@ fp12_element read_fp12(boost::json::value& input_json_value, std::size_t positio
         A[i] = parse_scalar<BlueprintFieldType>(arr[i].as_object().at("field"));
     }
     fp12_element res = fp12_element({ {A[0],A[1]}, {A[2],A[3]}, {A[4],A[5]} }, { {A[6],A[7]}, {A[8],A[9]}, {A[10],A[11]} });
+    return res;
+}
 
+
+template<typename BlueprintFieldType, typename group_type>
+typename group_type::value_type read_g2_curve(boost::json::value& input_json_value, std::size_t position) {
+
+    std::array<typename BlueprintFieldType::value_type, 4> A;
+
+    const boost::json::object &current_obj = input_json_value.as_array()[position].as_object();
+    ASSERT(current_obj.size() == 1 && current_obj.contains("vector"));
+    ASSERT(current_obj.at("vector").is_array());
+    auto &arr = current_obj.at("vector").as_array();
+
+    for(std::size_t i = 0; i < 4; i++) {
+        ASSERT(arr[i].as_object().contains("field"));
+        A[i] = parse_scalar<BlueprintFieldType>(arr[i].as_object().at("field"));
+    }
+
+    typedef typename group_type::value_type group_value_type;
+    typedef typename group_type::field_type::value_type field_value_type;
+
+    group_value_type res = group_value_type(
+        field_value_type(A[0], A[1]),
+        field_value_type(A[2], A[3]),
+        field_value_type::one()
+    );
     return res;
 }
