b057f40892e51f952bba9a963449dab8176dc1d4
Aleksandr Cherenkov <aleksandr.cherenkov.3@gmail.com>
2023-12-12 06:18:53 +0000

    add poseidon merkle tree examples with multiproving and private inputs #420

---
+++ examples/cpp/merkle_poseidon_2provers.cpp
@@ -0,0 +1,70 @@
+
+#include <nil/crypto3/hash/algorithm/hash.hpp>
+#include <nil/crypto3/hash/poseidon.hpp>
+
+using namespace nil::crypto3;
+using namespace nil::crypto3::algebra::curves;
+
+
+bool check_root (
+    typename pallas::base_field_type::value_type expected_root,
+    [[private_input]] std::array<typename pallas::base_field_type::value_type, 0x20> layer_0_leaves) {
+
+        std::array<typename pallas::base_field_type::value_type, 0x10> layer_1_leaves;
+        std::size_t layer_1_size = 0x10;
+        std::array<typename pallas::base_field_type::value_type, 0x8> layer_2_leaves;
+        std::size_t layer_2_size = 0x8;
+        std::array<typename pallas::base_field_type::value_type, 0x4> layer_3_leaves;
+        std::size_t layer_3_size = 0x4;
+        std::array<typename pallas::base_field_type::value_type, 0x2> layer_4_leaves;
+        std::size_t layer_4_size = 0x2;
+        typename pallas::base_field_type::value_type root;
+
+        for (std::size_t leaf_index = 0; leaf_index < layer_1_size; leaf_index++) {
+        layer_1_leaves[leaf_index] =
+            hash<hashes::poseidon>(layer_0_leaves[2 * leaf_index], layer_0_leaves[2 * leaf_index + 1]);
+    }
+
+    for (std::size_t leaf_index = 0; leaf_index < layer_2_size; leaf_index++) {
+        layer_2_leaves[leaf_index] =
+            hash<hashes::poseidon>(layer_1_leaves[2 * leaf_index], layer_1_leaves[2 * leaf_index + 1]);
+    }
+
+    for (std::size_t leaf_index = 0; leaf_index < layer_3_size; leaf_index++) {
+        layer_3_leaves[leaf_index] =
+            hash<hashes::poseidon>(layer_2_leaves[2 * leaf_index], layer_2_leaves[2 * leaf_index + 1]);
+    }
+
+    for (std::size_t leaf_index = 0; leaf_index < layer_4_size; leaf_index++) {
+        layer_4_leaves[leaf_index] =
+            hash<hashes::poseidon>(layer_3_leaves[2 * leaf_index], layer_3_leaves[2 * leaf_index + 1]);
+    }
+
+    typename pallas::base_field_type::value_type real_root = hash<hashes::poseidon>(layer_4_leaves[0], layer_4_leaves[1]);
+
+    bool res = (real_root == expected_root);
+    __builtin_assigner_exit_check(res);
+
+    return res;
+}
+
+[[circuit]] bool merkle_tree_poseidon (
+    typename pallas::base_field_type::value_type expected_root1,
+    [[private_input]] std::array<typename pallas::base_field_type::value_type, 0x20> layer_0_leaves1,
+    typename pallas::base_field_type::value_type expected_root2,
+    [[private_input]] std::array<typename pallas::base_field_type::value_type, 0x20> layer_0_leaves2
+) {
+
+    bool res1 = check_root(expected_root1, layer_0_leaves1);
+    __builtin_assigner_exit_check(res1);
+
+    bool res2;
+
+#pragma zk_multi_prover 1
+    {
+        res2 = check_root(expected_root2, layer_0_leaves2);
+        __builtin_assigner_exit_check(res2);
+    }
+
+    return res1 & res2;
+}
