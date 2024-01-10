ab7f5770feebff43d73c2d18bb0c053f18418e47
Mikhail Aksenov <maksenov@nil.foundation>
2024-01-10 10:33:00 +0000

    Move stdlib into main package, fix symlink creation on install

--- cmake/create-symlinks.cmake
+++ cmake/create-symlinks.cmake
@@ -3,17 +3,25 @@ message("Creating symlinks")
 list(APPEND BINARIES assigner transpiler)
 list(APPEND LLVM_BINARIES clang llvm-link)
 
-file(MAKE_DIRECTORY $ENV{DESTDIR}/usr/bin)
+# Link directory would differ for cases when we have DESTDIR from CPack or simple installation
+if (DEFINED ENV{DESTDIR})
+    set(LINK_DESTDIR $ENV{DESTDIR}/usr/bin)
+else()
+    set(LINK_DESTDIR ${CMAKE_INSTALL_PREFIX}/bin)
+endif()
+
+file(MAKE_DIRECTORY ${LINK_DESTDIR})
+message("LINK_DESTDIR ${LINK_DESTDIR}")
 
 foreach(BINARY IN LISTS BINARIES)
-    set(LINK_NAME $ENV{DESTDIR}/usr/bin/${BINARY})
+    set(LINK_NAME ${LINK_DESTDIR}/${BINARY})
     set(LINK_TARGET "../lib/zkllvm/bin/${BINARY}")
     message("${LINK_NAME} -> ${LINK_TARGET}")
     file(CREATE_LINK ${LINK_TARGET} ${LINK_NAME} SYMBOLIC)
 endforeach()
 
 foreach(BINARY IN LISTS LLVM_BINARIES)
-    set(LINK_NAME $ENV{DESTDIR}/usr/bin/${BINARY}-zkllvm)
+    set(LINK_NAME ${LINK_DESTDIR}/${BINARY}-zkllvm)
     set(LINK_TARGET "../lib/zkllvm/bin/${BINARY}")
     message("${LINK_NAME} -> ${LINK_TARGET}")
     file(CREATE_LINK ${LINK_TARGET} ${LINK_NAME} SYMBOLIC)
