3d1449efd63bcc7ef02b92d53ab30fe65ae8806c
e.tatuzova <e.tatuzova@nil.foundation>
2024-01-10 10:27:45 +0400

    Apped _assign to build example targets in gen_and_check.sh #177

--- gen_and_check.sh
+++ gen_and_check.sh
@@ -1,10 +1,9 @@
 set -e
 mkdir -p ./recursive
-ninja -C build arithmetics_cpp_example -j4
+ninja -C build arithmetics_cpp_example_assign -j10
 ./build/bin/recursive_gen/recursive_gen -m gen-input -i ./examples/inputs/arithmetics.inp -t ./build/examples/cpp/assignment_arithmetics_cpp_example.tbl -c ./build/examples/cpp/circuit_arithmetics_cpp_example.crct -o ./recursive -e pallas
 ./build/bin/recursive_gen/recursive_gen -m gen-verifier -i ./examples/inputs/arithmetics.inp -t ./build/examples/cpp/assignment_arithmetics_cpp_example.tbl -c ./build/examples/cpp/circuit_arithmetics_cpp_example.crct -o ./recursive -e pallas
-
-#ninja -C build merkle_tree_poseidon_cpp_example -j4
+#ninja -C build merkle_tree_poseidon_cpp_example_assign -j10
 #./build/bin/recursive_gen/recursive_gen -m gen-input -i ./examples/inputs/merkle_tree_poseidon.inp -t ./build/examples/cpp/assignment_merkle_tree_poseidon_cpp_example.tbl -c ./build/examples/cpp/circuit_merkle_tree_poseidon_cpp_example.crct -o ./recursive -e pallas
 #./build/bin/recursive_gen/recursive_gen -m gen-verifier -i ./examples/inputs/merkle_tree_poseidon.inp -t ./build/examples/cpp/assignment_merkle_tree_poseidon_cpp_example.tbl -c ./build/examples/cpp/circuit_merkle_tree_poseidon_cpp_example.crct -o ./recursive -e pallas
 
