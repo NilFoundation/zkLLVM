fcf5cc56716c9ac2011ec7b91db45a70224cef93
e.tatuzova <e.tatuzova@nil.foundation>
2024-02-12 13:31:40 +0400

    LPC batching updated #510

--- multi_gen_and_check.sh
+++ multi_gen_and_check.sh
@@ -1,6 +1,6 @@
 set -e
 mkdir -p ./recursive
-ninja -C build merkle_poseidon_3provers_cpp_example_assign -j10
+ninja -C build merkle_poseidon_3provers_cpp_example_assign -j4
 ./build/bin/recursive_gen/recursive_gen -m gen-input -i ./examples/inputs/merkle_poseidon_3provers_public.inp -t ./build/examples/cpp/assignment_merkle_poseidon_3provers_cpp_example.tbl0 -c ./build/examples/cpp/circuit_merkle_poseidon_3provers_cpp_example.crct0 -o ./recursive -e pallas -p 1 -s 3 --multi-prover
 ./build/bin/recursive_gen/recursive_gen -m gen-verifier -i ./examples/inputs/merkle_poseidon_3provers_public.inp -t ./build/examples/cpp/assignment_merkle_poseidon_3provers_cpp_example.tbl0 -c ./build/examples/cpp/circuit_merkle_poseidon_3provers_cpp_example.crct0 -o ./recursive -e pallas -p 1 -s 3 --multi-prover
 
