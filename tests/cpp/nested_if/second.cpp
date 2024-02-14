380feed90d100282ea049a3f7aabffbc619b124b
Aleksandr Cherenkov <aleksandr.cherenkov.3@gmail.com>
2024-02-14 12:56:16 +0000

    add nested_if test #527

---
+++ tests/cpp/nested_if/second.cpp
@@ -0,0 +1,84 @@
+#ifndef __ZKLLVM__
+#include "../read_boost_json.hpp"
+#include <fstream>
+#endif
+
+#include <nil/crypto3/algebra/curves/pallas.hpp>
+
+using namespace nil::crypto3::algebra::curves;
+
+[[circuit]] typename pallas::base_field_type::value_type
+    pallas_field_add(typename pallas::base_field_type::value_type a,
+                        typename pallas::base_field_type::value_type b) {
+
+#ifdef __ZKLLVM__
+    if (a > 2) {
+        if (b > 2) {
+            return a + b;
+        }
+        else {
+            return a - b;
+        }
+    }
+    else {
+        if (b < 2) {
+            return a * b;
+        }
+        else {
+            return a / b;
+        }
+    }
+    return 0;
+#else
+
+    typename pallas::base_field_type::value_type c;
+
+    if (a > 2) {
+        if (b > 2) {
+            c = a + b;
+            std::cout << c.data <<std::endl;
+            return c;
+        }
+        else {
+            c = a - b;
+            std::cout << c.data <<std::endl;
+            return c;
+        }
+    }
+    else {
+        if (b < 2) {
+            c = a * b;
+            std::cout << c.data <<std::endl;
+            return c;
+        }
+        else {
+            c = a / b;
+            std::cout << c.data <<std::endl;
+            return c;
+        }
+    }
+
+    c = typename pallas::base_field_type::value_type(0);
+    std::cout << c.data <<std::endl;
+    return c;
+#endif
+}
+
+#ifndef __ZKLLVM__
+
+int main (int argc, char *argv[]){
+    if (argc != 2) {
+        std::cerr << "one command line argument must be provided\n";
+        std::abort();
+    }
+
+    boost::json::value input_json = read_boost_json(std::string(argv[1]));
+
+    using BlueprintFieldType = typename pallas::base_field_type;
+    typename BlueprintFieldType::value_type a = read_field <BlueprintFieldType>(input_json, 0);
+    typename BlueprintFieldType::value_type b = read_field <BlueprintFieldType>(input_json, 1);
+
+    pallas_field_add(a, b);
+    return 0;
+}
+#endif
