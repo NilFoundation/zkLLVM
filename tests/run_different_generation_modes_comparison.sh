4e47fec8839d33e65a9c13653a0898a9c2a4dc98
Aleksandr Cherenkov <aleksandr.cherenkov.3@gmail.com>
2024-04-01 18:35:59 +0000

    update tests/run_different_generation_modes_comparison.sh

--- tests/run_different_generation_modes_comparison.sh
+++ tests/run_different_generation_modes_comparison.sh
@@ -1,8 +1,10 @@
 rm -r build/examples/cpp
-/usr/bin/cmake --build "$(pwd)/build" --config Debug --target cpp_examples_generate_tbl --
+rm -r build/1
+rm -r build/2
+/usr/bin/cmake --build "$(pwd)/build" --config Debug --target cpp_examples_generate_tbl_no_check --
 /usr/bin/cmake --build "$(pwd)/build" --config Debug --target cpp_examples_generate_crct --
 bash tests/copy_assigner_outputs.sh build/1
-/usr/bin/cmake --build "$(pwd)/build" --config Debug --target cpp_examples_generate_both --
+/usr/bin/cmake --build "$(pwd)/build" --config Debug --target cpp_examples_generate_both_no_check --
 bash tests/copy_assigner_outputs.sh build/2
 
 bash tests/compare_folders_content.sh build/1 build/2
