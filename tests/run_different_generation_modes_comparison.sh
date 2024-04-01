a7afb205bda2d596e4389f8e44c6e499851c446f
Aleksandr Cherenkov <aleksandr.cherenkov.3@gmail.com>
2024-04-01 18:22:03 +0000

    fix typo in different gen nodes comp script

--- tests/run_different_generation_modes_comparison.sh
+++ tests/run_different_generation_modes_comparison.sh
@@ -2,7 +2,7 @@ rm -r build/examples/cpp
 /usr/bin/cmake --build "$(pwd)/build" --config Debug --target cpp_examples_generate_tbl --
 /usr/bin/cmake --build "$(pwd)/build" --config Debug --target cpp_examples_generate_crct --
 bash tests/copy_assigner_outputs.sh build/1
-/usr/bin/cmake --build "$(pwd)/build" --config Debug --target cpp_examples_generate_crct --
+/usr/bin/cmake --build "$(pwd)/build" --config Debug --target cpp_examples_generate_both --
 bash tests/copy_assigner_outputs.sh build/2
 
 bash tests/compare_folders_content.sh build/1 build/2
