58120340aecfbccae6f6e998f25a9e2cc8de1c83
Andrei Malashkin <andrei.malashkin@nil.foundation>
2024-07-11 08:55:35 +0200

    refactor run_tests script

--- run_tests.sh
+++ run_tests.sh
@@ -1,50 +1,67 @@
 #!/usr/bin/env bash
 
-          export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$(pwd)/libs/circifier/llvm/lib"
-          echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/libs/circifier/llvm/lib"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$(pwd)/libs/circifier/llvm/lib"
+echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/libs/circifier/llvm/lib"
 
-          ninja -k 0 compile_rust_examples
-          ls -al ./examples/rust/target/assigner-unknown-unknown/release/examples
+echo "Build IR of the Rust examples"
+ninja -k 0 compile_rust_examples
+ls -al ./examples/rust/target/assigner-unknown-unknown/release/examples
 
-          ninja -k 0 rust_examples_generate_crct
-          ls -al ./examples/cpp
+echo "Build circuits(.crct) of the Rust examples"
+ninja -k 0 rust_examples_generate_crct
+ls -al ./examples/cpp
 
-          ninja -k 0 rust_examples_generate_tbl
-          ls -al ./examples/cpp
+echo "Build assignment tables(.tbl) of the Rust examples"
+ninja -k 0 rust_examples_generate_tbl
+ls -al ./examples/cpp
 
-          ninja -k 0 prove_rust_examples
+echo "Build proof for the circuit of the Rust examples"
+ninja -k 0 prove_rust_examples
 
-          ninja -k 0 compile_cpp_examples
-          ls -al ./examples/cpp
+echo "Build IR of the C++ examples"
+ninja -k 0 compile_cpp_examples
+ls -al ./examples/cpp
 
-          ninja -k 0 cpp_examples_generate_crct
-          ls -al ./examples/cpp
+echo "Build circuits(.crct) of the C++ examples"
+ninja -k 0 cpp_examples_generate_crct
+ls -al ./examples/cpp
 
-          ninja -k 0 cpp_examples_generate_tbl_no_check
-          ls -al ./examples/cpp
+echo "Build assignment tables(.tbl) of the C++ examples"
+ninja -k 0 cpp_examples_generate_tbl_no_check
+ls -al ./examples/cpp
 
-          bash ../tests/copy_assigner_outputs.sh ./examples/cpp/ separate_generatuon
+echo "Copy assigner output from separate generation mode"
+bash ../tests/copy_assigner_outputs.sh ./examples/cpp/ separate_generatuon
 
-          ninja -k 0 cpp_examples_generate_both
-          ls -al ./examples/cpp
+echo "Run simultaneous .tbl and .crct generation of the C++ examples"
+ninja -k 0 cpp_examples_generate_both
+ls -al ./examples/cpp
 
-          bash ../tests/copy_assigner_outputs.sh ./examples/cpp/ simultaneous_generation
+echo "Copy assigner output from simultaneous generation mode"
+bash ../tests/copy_assigner_outputs.sh ./examples/cpp/ simultaneous_generation
 
-          bash ../tests/compare_folders_content.sh separate_generatuon simultaneous_generation
+echo "Compare different assigner modes output"
+bash ../tests/compare_folders_content.sh separate_generatuon simultaneous_generation
 
-          ninja -k 0 cpp_examples_estimate_size
+echo "Run size estimation for C++ examples"
+ninja -k 0 cpp_examples_estimate_size
 
-          chmod +x ./examples/copy_artifacts.sh
-          ./examples/copy_artifacts.sh examples/cpp examples_output
-          ls -al examples_output/
+echo "Copy examples' circuits and assignments for uploading"
+chmod +x ./examples/copy_artifacts.sh
+bash ../examples/copy_artifacts.sh examples/cpp examples_output
+ls -al examples_output/
 
-          ninja -k 0 all_tests_compile_as_cpp_code
+echo "Compile tests as cpp code"
+ninja -k 0 all_tests_compile_as_cpp_code
 
-          ninja -k 0 all_tests_compile_as_circuits
+echo "Compile tests as circuits"
+ninja -k 0 all_tests_compile_as_circuits
 
-          ninja -k 0 all_tests_run_expected_res_calculation
+echo "Run tests as cpp code (expected res calculation)"
+ninja -k 0 all_tests_run_expected_res_calculation
 
-          INTEGRATION_TESTING_TARGETS=(
+echo "Get transpiler output for integration testing"
+INTEGRATION_TESTING_TARGETS=(
 	  "arithmetics_cpp_example_evm_verifier"
 	  "polynomial_cpp_example_evm_verifier"
 	  "poseidon_cpp_example_evm_verifier"
@@ -55,28 +72,36 @@
 	  "uint_bit_composition_evm_verifier"
 	  "compare_eq_cpp_evm_verifier"
 	  "private_input_cpp_evm_verifier"
-          )
+)
 
-          targets_str=$(echo "${INTEGRATION_TESTING_TARGETS}" | awk 'NF {printf " %s_evm_verifier ", $0}' | sed 's/[[:space:]]*$//')
-          echo "targets: ${targets_str}"
-          ninja -k 0  ${targets_str}
-          ls -al examples/cpp/
+targets_str=$(echo "${INTEGRATION_TESTING_TARGETS}" | awk 'NF {printf " %s_evm_verifier ", $0}' | sed 's/[[:space:]]*$//')
+echo "targets: ${targets_str}"
+ninja -k 0  ${targets_str}
+ls -al examples/cpp/
 
-          ninja -k 0 all_tests_assign_circuits
+echo "Run tests as circuits (real res calculation)"
+ninja -k 0 all_tests_assign_circuits
 
-          chmod +x ./tests/run_test_results_comparison.sh
-          bash ../tests/run_test_results_comparison.sh
+echo "Compare expected and real test results"
+chmod +x ./tests/run_test_results_comparison.sh
+bash ../tests/run_test_results_comparison.sh
 
-          chmod +x ./tests/run_tests_faulty_input.sh
-          bash ../tests/run_tests_faulty_input.sh pallas
+echo "Run tests on faulty inputs"
+chmod +x ./tests/run_tests_faulty_input.sh
+bash ../tests/run_tests_faulty_input.sh pallas
 
-          ninja -k 0 check-crypto3-assigner
+echo "Run assigner unit tests"
+ninja -k 0 check-crypto3-assigner
 
-          ninja -k 0 prove_cpp_examples
+echo "Build proof for the circuit of the C++ examples"
+ninja -k 0 prove_cpp_examples
 
-          ninja -k 0 recursive_gen
+echo "Build recursive gen"
+ninja -k 0 recursive_gen
 
-          ninja -k 0 compile_and_run_transpiler_tests
+echo "Build and run transpiler test"
+ninja -k 0 compile_and_run_transpiler_tests
 
-          ninja -k 0 recursion
+echo "Run recursive verifier tests"
+ninja -k 0 recursion
 
