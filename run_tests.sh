5c05b8489fa9ab104cef055943c41bf12fcaa2f1
Andrei Malashkin <andrei.malashkin@nil.foundation>
2024-07-12 10:41:55 +0200

    add info about memory usage, to be removed

--- run_tests.sh
+++ run_tests.sh
@@ -1,63 +1,80 @@
 #!/usr/bin/env bash
 
+df -h 
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$(pwd)/libs/circifier/llvm/lib"
 echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/libs/circifier/llvm/lib"
 
 echo "Build IR of the Rust examples"
+df -h
 ninja -k 0 compile_rust_examples
 ls -al ./examples/rust/target/assigner-unknown-unknown/release/examples
 
 echo "Build circuits(.crct) of the Rust examples"
+df -h
 ninja -k 0 rust_examples_generate_crct
 ls -al ./examples/cpp
 
 echo "Build assignment tables(.tbl) of the Rust examples"
+df -h
 ninja -k 0 rust_examples_generate_tbl
 ls -al ./examples/cpp
 
 echo "Build proof for the circuit of the Rust examples"
+df -h
 ninja -k 0 prove_rust_examples
 
 echo "Build IR of the C++ examples"
+df -h
 ninja -k 0 compile_cpp_examples
 ls -al ./examples/cpp
 
 echo "Build circuits(.crct) of the C++ examples"
+df -h
 ninja -k 0 cpp_examples_generate_crct
 ls -al ./examples/cpp
 
 echo "Build assignment tables(.tbl) of the C++ examples"
+df -h
 ninja -k 0 cpp_examples_generate_tbl_no_check
 ls -al ./examples/cpp
 
 echo "Copy assigner output from separate generation mode"
+df -h
 bash ../tests/copy_assigner_outputs.sh ./examples/cpp/ separate_generatuon
 
 echo "Run simultaneous .tbl and .crct generation of the C++ examples"
+df -h
 ninja -k 0 cpp_examples_generate_both
 ls -al ./examples/cpp
 
 echo "Copy assigner output from simultaneous generation mode"
+df -h
 bash ../tests/copy_assigner_outputs.sh ./examples/cpp/ simultaneous_generation
 
 echo "Compare different assigner modes output"
+df -h
 bash ../tests/compare_folders_content.sh separate_generatuon simultaneous_generation
 
 echo "Run size estimation for C++ examples"
+df -h
 ninja -k 0 cpp_examples_estimate_size
 
 echo "Copy examples' circuits and assignments for uploading"
+df -h
 chmod +x ./examples/copy_artifacts.sh
 bash ../examples/copy_artifacts.sh examples/cpp examples_output
 ls -al examples_output/
 
 echo "Compile tests as cpp code"
+df -h
 ninja -k 0 all_tests_compile_as_cpp_code
 
 echo "Compile tests as circuits"
+df -h
 ninja -k 0 all_tests_compile_as_circuits
 
 echo "Run tests as cpp code (expected res calculation)"
+df -h
 ninja -k 0 all_tests_run_expected_res_calculation
 
 echo "Get transpiler output for integration testing"
@@ -76,33 +93,42 @@ INTEGRATION_TESTING_TARGETS=(
 
 targets_str=$(echo "${INTEGRATION_TESTING_TARGETS}" | awk 'NF {printf " %s_evm_verifier ", $0}' | sed 's/[[:space:]]*$//')
 echo "targets: ${targets_str}"
+df -h
 ninja -k 0  ${targets_str}
 ls -al examples/cpp/
 
 echo "Run tests as circuits (real res calculation)"
+df -h
 ninja -k 0 all_tests_assign_circuits
 
 echo "Compare expected and real test results"
+df -h
 chmod +x ./tests/run_test_results_comparison.sh
 bash ../tests/run_test_results_comparison.sh
 
 echo "Run tests on faulty inputs"
+df -h
 chmod +x ./tests/run_tests_faulty_input.sh
 bash ../tests/run_tests_faulty_input.sh pallas
 
 echo "Run assigner unit tests"
+df -h
 ninja -k 0 check-crypto3-assigner
 
 echo "Build proof for the circuit of the C++ examples"
+df -h
 ninja -k 0 prove_cpp_examples
 
 echo "Build recursive gen"
+df -h
 ninja -k 0 recursive_gen
 
 echo "Build and run transpiler test"
+df -h
 ninja -k 0 compile_and_run_transpiler_tests
 
 echo "Run recursive verifier tests"
+df -h
 ninja -k 0 recursion
 
 echo "finish"
