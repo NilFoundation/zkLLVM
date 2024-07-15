da5fdcec80a469120167fb97f92ae5eb592c4643
Andrei Malashkin <andrei.malashkin@nil.foundation>
2024-07-15 14:49:53 +0200

    move assigner output

--- run_tests.sh
+++ run_tests.sh
@@ -17,9 +17,9 @@ df -h
 ninja -k 0 cpp_examples_generate_tbl_no_check
 ls -al ./examples/cpp
 
-echo "Copy assigner output from separate generation mode"
+echo "Move assigner output from separate generation mode"
 df -h
-bash ../tests/copy_assigner_outputs.sh ./examples/cpp/ ${ARTIFACTS_DIR}separate_generation
+bash ../tests/move_assigner_outputs.sh ./examples/cpp/ ${ARTIFACTS_DIR}separate_generation
 ls -al ${ARTIFACTS_DIR}separate_generation
 
 echo "Run simultaneous .tbl and .crct generation of the C++ examples"
